f1ef85eae61f046d13183ad628ce1a15dabea041
Saikumar Konakanchi <120327792+SaikumarKonakanchi0@users.noreply.github.com>
2023-06-27 20:29:06 +0530

    Fibo_Series.py
    The provided both codes gives the same output which is first 50 Fibonacci Series

---
+++ Fibo_Series.py
@@ -0,0 +1,18 @@
+#Write a Python program to get the Fibonacci series between 0 to 50
+f=0
+n=1
+print(f)
+print(n)
+for i in range(2,51):
+    t=f+n
+    f=n
+    n=t
+    print(t)
+
+#Another better version of code
+fib_series=[0,1]
+for i in range(2,51):
+    next_num=fib_series[i-1]+fib_series[i-2]
+    fib_series.append(next_num)
+for i in fib_series:
+    print(i)
