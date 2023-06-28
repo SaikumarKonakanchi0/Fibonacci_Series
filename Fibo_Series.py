e1d95ad49b27440ed8ce4d7ea7606c5bd8365777
Saikumar Konakanchi <120327792+SaikumarKonakanchi0@users.noreply.github.com>
2023-06-28 19:21:38 +0530

    Fibo_Series.py

--- Fibo_Series.py
+++ Fibo_Series.py
@@ -3,7 +3,7 @@ f=0
 n=1
 print(f)
 print(n)
-for i in range(2,51):
+for i in range(2,50):
     t=f+n
     f=n
     n=t
@@ -11,7 +11,7 @@ for i in range(2,51):
 
 #Another better version of code
 fib_series=[0,1]
-for i in range(2,51):
+for i in range(2,50):
     next_num=fib_series[i-1]+fib_series[i-2]
     fib_series.append(next_num)
 for i in fib_series:
