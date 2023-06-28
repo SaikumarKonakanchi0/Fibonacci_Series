746f9b194464aeabbd673a73354748bee552c78e
Saikumar Konakanchi <120327792+SaikumarKonakanchi0@users.noreply.github.com>
2023-06-28 19:43:23 +0530

    Fibo_Series.py

--- Fibo_Series.py
+++ Fibo_Series.py
@@ -1,7 +1,6 @@
 #Write a Python program to get the Fibonacci series between 0 to 50
 f=0
 n=1
-print(f)
 print(n)
 for i in range(2,20):
     t=f+n
@@ -16,5 +15,5 @@ for i in range(2,20):
     next_num=fib_series[i-1]+fib_series[i-2]
     fib_series.append(next_num)
 for i in fib_series:
-    if i<=50:
+    if i<=50 and i!=0:
         print(i)
